2ef30b6fee923f33f1038ddc45ac0ab733994f4b
Naveen_Kumar24 <51779899+naveenkumar2405@users.noreply.github.com>
2019-08-05 17:24:45 +0530

    Update CK29.cpp

--- CK29.cpp
+++ CK29.cpp
@@ -1,13 +1,11 @@
 #include <iostream>
-
 using namespace std;
 
 int main ()
 {
     float hrs, min;
     cin >> min;
-    hrs= min*(1/60);
-
+    hrs= min*0.016667);
     cout << "\n" << hrs << " " << min ;
     return 0;
 }
