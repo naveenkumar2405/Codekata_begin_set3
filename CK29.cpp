51b146ff3827592e899899557d7be56d69886c32
Naveen_Kumar24 <51779899+naveenkumar2405@users.noreply.github.com>
2019-08-05 17:17:56 +0530

    Create CK29.cpp

---
+++ CK29.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main ()
+{
+    float hrs, min;
+    cin >> min;
+    hrs= min*(1/60);
+
+    cout << "\n" << hrs << " " << min ;
+    return 0;
+}
