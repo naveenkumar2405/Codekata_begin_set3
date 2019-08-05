f21cef198bfcd4f2dba10b2d46bcf597e58a6fa5
Naveen_Kumar24 <51779899+naveenkumar2405@users.noreply.github.com>
2019-08-05 18:55:35 +0530

    Create CK23.cpp

---
+++ CK23.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main ()
+{
+    int arr[50], n, i, min;
+
+    cin >> n;
+
+    for (i = 0; i < n; i++)
+        cin >> arr[i];
+
+    min = arr[0];
+    for (i = 0; i < n; i++)
+    {
+        if (min > arr[i])
+            min = arr[i];
+    }
+
+    cout << min;
+    return 0;
+}
