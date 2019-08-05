a4b8920e646ff881b6104a95567a02a9e3695386
Naveen_Kumar24 <51779899+naveenkumar2405@users.noreply.github.com>
2019-08-05 19:06:55 +0530

    Create CK24.cpp

---
+++ CK24.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+using namespace std;
+
+int main ()
+{
+    int arr[50], n, i, j;
+
+    cin >> n;
+
+    for (i = 0; i < n; i++)
+        cin >> arr[i];
+
+	for (i = 0; i < n; ++i)
+	{
+		for (j = 0; j < n-i-1; ++j)
+		{
+
+			if (arr[j] > arr[j+1])
+			{
+				arr[j] = arr[j]+arr[j+1];
+				arr[j+1] = arr[j]-arr[j + 1];
+				arr[j] = arr[j]-arr[j + 1];
+			}
+		}
+
+	}
+
+
+    for (i = 0; i < n; i++)
+        cout<<" "<<arr[i];
+    return 0;
+}
